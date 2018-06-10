b254cd79af078676376118fa9b8e7d9dea5073db
Elir de Assis Ribeiro <ellirweb@gmail.com>
2018-06-09 21:34:48 -0300

    fazendo upload

--- Biblioteca/src/Web/Scripts/angular/controller/biblioteca.js
+++ Biblioteca/src/Web/Scripts/angular/controller/biblioteca.js
@@ -1,6 +1,31 @@
 ﻿biblioteca.controller('authctrl', ['$scope', 'biblioservi', function ($scope, biblioservi) {
     $scope.Registrar = function () {
-        this.biblioservi.Registrar($scope.formcadastro);
+        var dataobj = {
+            RazaoSocial: $scope.RazaoSocial,
+            Usuario: $scope.Usuario,
+            Senha: $scope.Senha,
+            Email: $scope.Email,
+            Email: $scope.Email,
+            Cnpj: $scope.Cnpj,
+
+
+        };
+        var res = this.biblioservi.Registrar(dataobj);
+        res.success(function (data, status, headers, config) {
+            alert("Cadastro feito com sucesso");
+        });
+        
+        res.error(function (data, status, headers, config) {
+            alert("Erro no cadastro");
+        });
+
+        $scope.RazaoSocial = "";
+        $scope.Usuario = "";
+        $scope.Usuario = "",
+        $scope.Senha = "";
+        $scope.Email = "";
+        $scope.Email = "";
+        $scope.Cnpj = "";
     }
 
 
